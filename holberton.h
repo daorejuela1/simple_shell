c38e4d2dabafc59cb921cc2175018f6369c2d89b
KarenCampo777 <1472@holbertonschool.com>
2020-04-10 14:44:12 +0000

    adding header file for the Super-Shell Project

---
+++ holberton.h
@@ -0,0 +1,27 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/*  space to include libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/* space for function prototypes*/
+int process_selector(char **command, int status)
+
+/* space for structure definition*/
+
+extern char **environ;
+stat
+
+/* space for auxiliar funcitons*/
+char *_getenv(const char *name);
+
+/* space for store paths*/
+
+#endif  
