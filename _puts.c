f0246801d686e651b88f85e728730a140f840cf8
KarenCampo777 <1472@holbertonschool.com>
2020-04-11 20:59:33 +0000

    added main_puts and _putchar new files

---
+++ _puts.c
@@ -0,0 +1,17 @@
+#include "holberton.h"	
+/**	
+ * _puts - writes a string in stdout	
+ * @s: The string to print	
+ * Return: On length of characters written	
+ */	
+int _puts(char *s)	
+{	
+	int length = 0;	
+
+	while (*(s + length) != 0)	
+	{	
+		_putchar(*(s + length));	
+		length++;	
+	}	
+	return (length);	
+}
