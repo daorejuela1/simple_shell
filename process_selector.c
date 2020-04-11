adcdc4a5190505f22c9a578123c422fcbe5f7993
KarenCampo777 <1472@holbertonschool.com>
2020-04-11 20:52:59 +0000

    first merge to test with main

---
+++ process_selector.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * process_selector - function that selects the children processes
+ * @command: buffer variable where data is stored temporarily
+ * @status: returns children running status
+ * Return: Always 0 on success
+*/
+
+int process_selector(char **command, int *status)
+{
+	pid_t children;
+
+	children = fork();
+	if (children < 0) /*Error*/
+	{
+		perror("Error: Children process can't be created");
+		exit(1);
+	}
+	if (children == 0) /*Children process*/
+	{
+		printf("El comando es: %s\n", command[0]);
+		if (execve(command[0], command, NULL) == -1)
+			perror("No such file or directory");
+	}
+	if (children > 0) /*current process*/
+	{
+		wait(status);
+	}
+return (0);
+}
