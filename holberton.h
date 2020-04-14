ee3166ef2c2fe5531b1a732d6784babe7e12dd16
KarenCampo777 <1472@holbertonschool.com>
2020-04-14 21:35:16 +0000

    removed some forbiden functions

--- holberton.h
+++ holberton.h
@@ -70,7 +70,6 @@ int errno_per(char *name, int line, char *error);
 int errno_found(char *name, int line, char *error);
 char *path_searcher(char **command, char *env);
 char *_getenv(const char *name);
-void free_list(p_node *head);
 char *get_match(const p_node *h);
 int create_process(creator_args param);
 /*space for extern variables*/
