ac39dc65a92a102e57b096aed41a96e15043db96
KarenCampo777 <1472@holbertonschool.com>
2020-04-16 01:08:38 +0000

    fixing code

--- _putchar.c
+++ _putchar.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * _putchar - function that writes the character c to stdout
+ * @c: Character to print
  *
- * Return: On success 1.
+ * Return: 1 on success
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
